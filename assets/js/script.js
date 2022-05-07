9461d5e20fb9f67c24a7b24723c1251dd316890b
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2022-05-07 10:16:56 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const carousel = document.querySelector('ul.carousel-container');
+const previousButton = document.querySelector('button.previous');
+const nextButton = document.querySelector('button.next');
+
+previousButton.addEventListener('click',() => {
+    let scroll = carousel.scrollLeft;
+    carousel.scroll((scroll - carousel.clientWidth), 0)
+})
+
+nextButton.addEventListener('click',() => {
+    let scroll = carousel.scrollLeft;
+    carousel.scroll((scroll + carousel.clientWidth), 0)
+})
